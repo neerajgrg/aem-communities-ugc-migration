c49bdcdfac93af27cdd3f17fdcec660be7643fdc
neerajgrg <neerajgrg93@gmail.com>
2019-10-04 12:34:50 +0530

    Update BadgingConstants.java

--- bundles/communities-badges-migration/src/main/java/com/adobe/cq/social/badges/resource/migrator/internal/BadgingConstants.java
+++ bundles/communities-badges-migration/src/main/java/com/adobe/cq/social/badges/resource/migrator/internal/BadgingConstants.java
@@ -3,10 +3,8 @@ package com.adobe.cq.social.badges.resource.migrator.internal;
 public class BadgingConstants {
 	
 
-  
-     public static final   String COMPONENT_RESOURCE_PATH = "/content/siliconlabs/en/community/jcr:content";
-     
-   
+     // Change the path with the site path where the badgingRules and scoringRules are configured
+     public static final   String COMPONENT_RESOURCE_PATH = "/content/sites/test/en/jcr:content";
      
      public static final String LIBS_IMAGES_PATH = "/libs";
      
